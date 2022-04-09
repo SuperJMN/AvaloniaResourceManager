d6b7bbda3b3957a95b5c4a9c6b553c2b14889c2f
José Manuel Nieto <superjmn@outlook.com>
2022-04-10 00:19:44 +0200

    Generalize

--- Tests/ResourceFinderTests.cs
+++ Tests/ResourceFinderTests.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Avalonia.Diagnostics.ResourceTools.Core.Static;
 using Xunit;
 using CSharpFunctionalExtensions;
 using FileSystem;
 using MoreLinq;
 using Serilog;
+using FileSystem = System.IO.Abstractions.FileSystem;
 
 namespace Tests;
 
@@ -19,6 +22,19 @@ public class ResourceFinderTests
     public async Task Extract()
     {
         var resources = await GetAllResources(Scheduler.CurrentThread);
+    }
+
+    [Fact]
+    public async Task Extract2()
+    {
+        var fileProcessor = new FileProcessor<ColdResource>(stream =>
+            {
+                var resFinder = new ResourceFinder();
+                return resFinder.FindAll(stream);
+            }, DefaultScheduler.Instance, "E:\\Repos\\SuperJMN\\WalletWasabi",
+            new ZafiroFileSystem(new System.IO.Abstractions.FileSystem(), Maybe<ILogger>.None),
+            file => file.Path.ToString().EndsWith(".axaml"));
+        var resources = await fileProcessor.Execute();
     }
 
     private static Task<Result<IList<ColdResource>>> GetAllResources(IScheduler taskPoolScheduler)
@@ -50,5 +66,54 @@ public class ResourceFinderTests
     {
         return Observable.Using(file.OpenRead,
             s => Observable.Return(sut.FindAll(s).ToObservable(), scheduler ?? Scheduler.Default));
+    }
+}
+
+public class FileProcessor<T>
+{
+    private readonly Func<Stream, IEnumerable<T>> func;
+    private readonly IScheduler taskPoolScheduler;
+    private readonly ZafiroPath path;
+    private readonly ZafiroFileSystem zafiroFileSystem;
+    private readonly Func<IZafiroFile, bool> fileFilter;
+
+    public FileProcessor(Func<Stream, IEnumerable<T>> func, IScheduler taskPoolScheduler, ZafiroPath path, ZafiroFileSystem zafiroFileSystem, Func<IZafiroFile, bool> fileFilter)
+    {
+        this.func = func;
+        this.taskPoolScheduler = taskPoolScheduler;
+        this.path = path;
+        this.zafiroFileSystem = zafiroFileSystem;
+        this.fileFilter = fileFilter;
+    }
+
+    public Task<Result<IEnumerable<T>>> Execute()
+    {
+        return zafiroFileSystem
+            .GetDirectory(path)
+            .Map(SelectMany)
+            .Map(async observable => await observable);
+    }
+
+    private IObservable<IEnumerable<T>> SelectMany(IZafiroDirectory d)
+    {
+        return GetAllFiles(d)
+            .ToObservable()
+            .Where(fileFilter)
+            .SelectMany(file => ProcessFile(file, taskPoolScheduler));
+    }
+
+    private IEnumerable<IZafiroFile> GetAllFiles(IZafiroDirectory directory)
+    {
+        return MoreEnumerable
+            .TraverseBreadthFirst(directory, d => d.Directories)
+            .SelectMany(d => d.Files);
+    }
+
+    private IObservable<IEnumerable<T>> ProcessFile(IZafiroFile file, IScheduler? scheduler = default)
+    {
+        return Observable
+            .Using(
+                file.OpenRead,
+            s => Observable.Return(func(s), scheduler ?? Scheduler.Default));
     }
 }
