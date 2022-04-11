1a08289d77bf745f7f1071189a6ea320e84ba1e1
José Manuel Nieto <superjmn@outlook.com>
2022-04-11 09:41:35 +0200

    Load resources

---
+++ ResourceManager/ViewModels/ColdResourceViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reactive;
+using Avalonia.Diagnostics.ResourceTools.Core.Static;
+using Avalonia.Markup.Xaml;
+using Portable.Xaml;
+using ReactiveUI;
+
+namespace Avalonia.Diagnostics.ResourceTools.ViewModels;
+
+public class ColdResourceViewModel : ViewModelBase
+{
+    private readonly ObservableAsPropertyHelper<object> preview;
+
+    public ColdResourceViewModel(ColdResource resource)
+    {
+        Key = resource.Key;
+        Name = resource.Name;
+        Load = ReactiveCommand.Create(() =>
+        {
+            return new object();
+        });
+
+        preview = Load.ToProperty(this, x => x.Preview);
+
+        Load
+            .Execute()
+            .Subscribe(o => { });
+    }
+
+    public object Preview => preview.Value;
+
+    public ReactiveCommand<Unit, object> Load { get; }
+
+    public string Name { get; set; }
+
+    public string Key { get; set; }
+}
--- ResourceManager/ViewModels/MainViewModel.cs
+++ ResourceManager/ViewModels/MainViewModel.cs
@@ -1,26 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using Avalonia.Diagnostics.ResourceTools.Core.Static;
 using CSharpFunctionalExtensions;
 using DynamicData;
+using Portable.Xaml;
 using ReactiveUI;
 
 namespace Avalonia.Diagnostics.ResourceTools.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private readonly ReadOnlyObservableCollection<ColdResource> resources;
+    private readonly ReadOnlyObservableCollection<ColdResourceViewModel> resources;
 
     public MainViewModel(ResourceAnalizer analizer)
     {
-        Load = ReactiveCommand.CreateFromTask(() =>
-            analizer.GetAllResources(Scheduler.Default, "E:\\Repos\\SuperJMN\\WalletWasabi"));
+        Load = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var allResources = await analizer.GetAllResources(Scheduler.Default, "E:\\Repos\\SuperJMN\\WalletWasabi");
+            return allResources.Map(r =>
+            {
+                return r.Select(cr => new ColdResourceViewModel(cr)).ToList();
+            });
+        });
 
-        SourceCache<ColdResource, string> sourceCache = new(x => x.Key);
+        SourceCache<ColdResourceViewModel, string> sourceCache = new(x => x.Key);
         sourceCache.Connect()
             .Bind(out resources)
             .ObserveOn(RxApp.MainThreadScheduler)
@@ -29,7 +38,7 @@ public class MainViewModel : ViewModelBase
         Load.Subscribe(r => r.OnSuccessTry(list => sourceCache.Edit(x => x.Load(list))));
     }
 
-    public ReadOnlyObservableCollection<ColdResource> Resources => resources;
+    public ReadOnlyObservableCollection<ColdResourceViewModel> Resources => resources;
 
-    public ReactiveCommand<Unit, Result<IList<ColdResource>>> Load { get; }
+    public ReactiveCommand<Unit, Result<List<ColdResourceViewModel>>> Load { get; }
 }
